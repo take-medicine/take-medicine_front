631ef13729cc24c8347df8314866b399e47a43fc
Sara Vázquez <saravazlo@gmail.com>
2025-09-17 17:56:27 +0200

    feat: updated log in page logic with sign in

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,31 +1,41 @@
 import React, { useState } from 'react';
 import './LogInPage.css';
 import LogInModal from "../components/logInModal/LogInModal";
+import SignInModal from '../components/signInModal/SignInModal';
 
 export default function LogInPage() {
-    const [isModalOpen, setIsModalOpen] = useState(true);
-
+    const [activeModal, setActiveModal] = useState('login')
    
     const handleCloseModal = () => {
-        setIsModalOpen(false);
+        setActiveModal(null);
     };
 
     const handleLoginSuccess = () => {
         handleCloseModal();
     };
-
+    
     const handleGoToRegister = () => {
-        handleCloseModal();
+        setActiveModal('signin');
+    };
+
+    const handleGoToLogin = () => {
+        setActiveModal('login');
     };
 
     return (
         <div className="log-in__page">
-           
-            {isModalOpen && (
+            {activeModal === 'login' && (
                 <LogInModal
                     onClose={handleCloseModal}
                     onLoginSuccess={handleLoginSuccess}
                     onGoToRegister={handleGoToRegister}
+                />
+            )}
+
+            {activeModal === 'signin' && (
+                <SignInModal
+                    onCancel={handleGoToLogin}
+                    onSave={handleGoToLogin}
                 />
             )}
         </div>
