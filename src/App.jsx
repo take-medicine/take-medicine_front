3c021252af4bbeed1ace7b6c00d4da2ff5a00e6c
Sara Vázquez <saravazlo@gmail.com>
2025-09-17 17:49:28 +0200

    feat: added captcha to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import '../src/components/styles/Variables.css';
 import './App.css';
 import LogInPage from './pages/LogInPage';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+
+const RECAPTCHA_SITE_KEY = "6Lduq8wrAAAAALArzaoXKVG4QmBVgfhvQ4wxjL9m";
+
 function App() {
 
   return (
-    <>
-    <LogInPage />
-    </>
+    <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
+      <LogInPage />
+    </GoogleReCaptchaProvider>
   )
 }
 
