94909064cc817d686076bf163df802dbb884f7a1
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-09-18 15:50:02 +0200

    test: pass addButton

---
+++ src/test/AddButton.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import AddButton from "../components/addButton/AddButton.jsx";
+
+describe("AddButton component", () => {
+  it("renders the + button", () => {
+    render(<AddButton onGoToTreatment={() => {}} onGoToReminder={() => {}} />);
+    
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+
+  it("opens modal when clicked", () => {
+    render(<AddButton onGoToTreatment={() => {}} onGoToReminder={() => {}} />);
+    
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+
+    // comprueba que se renderizó el modal
+    expect(screen.getByText(/añadir tratamiento/i)).toBeInTheDocument();
+    expect(screen.getByText(/añadir recordatorio/i)).toBeInTheDocument();
+  });
+});
