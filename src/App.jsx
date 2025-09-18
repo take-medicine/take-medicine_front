cec7dd6e2f72b8b70caf2eef3aba39181f18222a
Sara Vázquez <saravazlo@gmail.com>
2025-09-18 13:48:19 +0200

    fix: cleaned app

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,14 @@ import '../src/components/styles/Variables.css';
 import './App.css';
 import LogInPage from './pages/LogInPage';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
-import TreatmentDetails from './components/treatmentDetails/TreatmentDetails';
 
 const RECAPTCHA_SITE_KEY = "6Lduq8wrAAAAALArzaoXKVG4QmBVgfhvQ4wxjL9m";
 
 function App() {
 
-  const mockTreatment = {
-    medicationName: "Insulina",
-    description: "Lo tomo para regular la cantidad de azúcar dos veces al día",
-    duration: "2 al día",
-    dosage: "Crónico",
-    time: ["09:00h", "17:00h"],
-    days: ["Lunes", "Miércoles", "Viernes"]
-  };
-
   return (
     <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
-      {/*<LogInPage /> */}
-      <TreatmentDetails treatment={mockTreatment} ></TreatmentDetails>
+      <LogInPage />
     </GoogleReCaptchaProvider>
 
     
