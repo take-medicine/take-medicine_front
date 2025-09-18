592712e602c3b769c15278a68f9272c2383d3ec2
Sara Vázquez <saravazlo@gmail.com>
2025-09-18 11:19:44 +0200

    feat: updated modal options with close function by clicking outside

--- src/components/modalOptions/ModalOptions.jsx
+++ src/components/modalOptions/ModalOptions.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './ModalOptions.css';
 
-export default function ModalOptions({onGoToTreatment, onGoToReminder}) {
+export default function ModalOptions({onGoToTreatment, onGoToReminder, onClose}) {
     return(
-        <div className="modal-options__overlay">
-            <div className="modal-options__container" role="dialog" aria-modal="true">
+        <div className="modal-options__overlay" onClick={onClose}>
+            <div className="modal-options__container" role="dialog" aria-modal="true" onClick={(e) => e.stopPropagation()}>
                 <div className="modal-options__actions">
                     <button type="button" className="modal-options__link" onClick={onGoToTreatment}>Añadir tratamiento</button>
                     <button type="button" className="modal-options__link" onClick={onGoToReminder}>Añadir recordatorio</button>
