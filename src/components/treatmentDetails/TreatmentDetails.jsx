b918ffc37b502bb802650fb21f88586d387360f3
Sara Vázquez <saravazlo@gmail.com>
2025-09-18 13:02:15 +0200

    fix: fixed some functionalities but it isnt working yet

--- src/components/treatmentDetails/TreatmentDetails.jsx
+++ src/components/treatmentDetails/TreatmentDetails.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import './TreatmentDetails.css';
+import '../pillDetail/PillDetail';
 
 export default function TreatmentDetails( { treatment, onClose }) {
+    if (!treatment) {
+        return null;
+      }
+
 return(
     <div className="treatment-details__overlay" onClick={onClose}>
         <div className="treatment-details__container" role="dialog" aria-modal="true" onClick={(e) => e.stopPropagation()}>
             <h3 className="treatment-details__name">{treatment.medicationName}</h3>
             <p className="treatment-details__description">{treatment.description}</p>
             <div className="treatment-details__info">
-                <span className="treament-details__subtitle">{treatment.duration}</span><span className="treament-details__subtitle"> | </span><span className="treament-details__subtitle">{treatment.dosage}</span>
+                <span className="treatment-details__subtitle">{treatment.duration}</span><span className="treatment-details__subtitle"> | </span><span className="treatment-details__subtitle">{treatment.dosage}</span>
             </div>
         <div className="treatment-details__section">
           {treatment.time.map((time, index) => (
-            <Pill key={index} label={time} type="hour" />
+            <PillDetail key={index} label={time} type="time" />
           ))}
         </div>
 
         <div className="treatment-details__section">
           <p className="treament-details__subtitle">Días de la semana</p>
           {treatment.days.map((day, index) => (
-            <Pill key={index} label={day} type="day" />
+            <PillDetail key={index} label={day} type="day" />
           ))}
         </div>
         </div>
