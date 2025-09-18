7c40b3b6d6980f76e48d75f7080182fdf5754a5b
Sara Vázquez <saravazlo@gmail.com>
2025-09-18 16:40:31 +0200

    feat: updated card resume

--- src/pages/Calendar/CalendarPage.jsx
+++ src/pages/Calendar/CalendarPage.jsx
@@ -3,6 +3,8 @@ import "./CalendarPage.css";
 import Calendar from "../../components/calendar/Calendar.jsx"
 import Navbar from "../../components/Navbar/Navbar";
 import HistoryCard from "../../components/historyCard/HistoryCard.jsx";
+import CardResume from "../../components/cardResume/cardResume.jsx";
+import TreatmentDetails from "../../components/treatmentDetails/TreatmentDetails.jsx";
 
 const CalendarPage = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
@@ -24,11 +26,13 @@ const CalendarPage = () => {
 
                 <div className="calendar-current">
                     <h2>Tomas de hoy</h2>
+                    <CardResume />
                 </div>
             </div>
 
             <div className="calendar-right">
                 <h2>Tratamiento actual</h2>
+                
             
                 <HistoryCard />
             </div>
