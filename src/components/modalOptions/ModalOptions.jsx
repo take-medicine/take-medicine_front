f483858a456499598828f3771f510816e0d4152a
Sara Vázquez <saravazlo@gmail.com>
2025-09-18 10:43:40 +0200

    styles: added styles to options modal

--- src/components/modalOptions/ModalOptions.jsx
+++ src/components/modalOptions/ModalOptions.jsx
@@ -3,10 +3,12 @@ import './ModalOptions.css';
 
 export default function ModalOptions({onGoToTreatment, onGoToReminder}) {
     return(
-        <div className="modal-options__container">
-            <div className="modal-options__actions">
-                <button type="button" className="modal-options__link" onClick={onGoToTreatment}>Añadir tratamiento</button>
-                <button type="button" className="modal-options__link" onClick={onGoToReminder}>Añadir recordatorio</button>
+        <div className="modal-options__overlay">
+            <div className="modal-options__container" role="dialog" aria-modal="true">
+                <div className="modal-options__actions">
+                    <button type="button" className="modal-options__link" onClick={onGoToTreatment}>Añadir tratamiento</button>
+                    <button type="button" className="modal-options__link" onClick={onGoToReminder}>Añadir recordatorio</button>
+                </div>
             </div>
         </div>
     );
